be131bef7d4a6e433cdc0c7d8464b00a0b3a98b9
jayeshuk <jayeshukalkar@gmail.com>
2022-08-18 18:14:08 +0530

    insertionSort complete

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -1,22 +1,43 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main(){
+int insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j;
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (current < arr[j])
+                arr[j + 1] = arr[j];
+            else
+                break;
+        }
+        arr[j + 1] = current;
+    }
+}
+
+int main()
+{
     int n;
-    cout<<"Insertion Sort"<<endl;
-    cout<<"Enter size of Array:";
-    cin>>n;
-    cout<<"Enter Array elements:";
+    cout << "Insertion Sort" << endl;
+    cout << "Enter size of Array:";
+    cin >> n;
+    cout << "Enter Array elements:";
 
-    int arr[100]={0};
+    int arr[100] = {0};
 
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
 
-    for(int i=0;i<10;i++){
-        cout<<arr[i]<<" ";
+    insertionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 
     return 0;
